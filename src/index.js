6b565550998c5f981691b851e70a94cbc43564c7
Akash Madhu <akashmadhu4@gmail.com>
2021-05-16 14:23:59 +0530

    intial template to work

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import express from "express";
+import exhbs from "express-handlebars";
+
+const STATIC_PATH = "assets";
+const app = express();
+dotenv.config()
+app.engine(
+    "handlebars",
+    exhbs.create({
+        partialsDir: 'views/partials',
+    }).engine);
+app.set("view engine", "handlebars");
+app.use(express.json())
+
+app.use(`/${STATIC_PATH}`, express.static('assets'));
+
+export const home = app.get("/", async (req, res) => {
+    try {
+        return res.render('home')
+    }
+    catch (err) {
+        console.log(JSON.stringify(err))
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening to Port Number:${process.env.PORT}` + " " + `http://localhost:${process.env.PORT}`)
+})
